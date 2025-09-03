272d8628a2aa5e96c00c0c10f2ca71a321d7d593
Bittu Ghosh. <bittughosh848@gmail.com>
2025-09-03 10:17:44 +0530

    validation added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const { check, validationResult } = require("express-validator");
 
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -20,12 +21,15 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/terms", (req, res) => {
+  res.render("terms");
+});
 app.get("/todos", isLoggedIn, async (req, res) => {
   const loggedInUser = await UserModel.find({ _id: req.user.userId });
   const todos = await TodoModel.find({ user: loggedInUser[0]._id }).populate(
     "user"
   );
-  res.render("todos", { todos, userName: loggedInUser[0].name });
+  res.render("todos", { todos, userFristName: loggedInUser[0].firstName });
 });
 app.post("/addtodo", isLoggedIn, async (req, res) => {
   const { todo, date } = req.body;
@@ -39,35 +43,114 @@ app.post("/addtodo", isLoggedIn, async (req, res) => {
   res.redirect("/todos");
 });
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { errorMag: null, email: "" });
 });
+
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
   res.redirect("/");
 });
 app.get("/signup", (req, res) => {
-  res.render("signup");
+  res.render("signup", { errors: [], oldInput: {} });
 });
-app.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(password, salt, async function (err, hashPassword) {
+
+app.post(
+  "/signup",
+
+  // Validation chain
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First Name should be at least 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First Name should contain only alphabets"),
+
+  check("lastName")
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage("Last Name should contain only alphabets"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password should be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain at least one number")
+    .matches(/[!@&]/)
+    .withMessage("Password should contain at least one special character")
+    .trim(),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("Please accept the terms and conditions")
+    .custom((value) => {
+      if (value !== "on") {
+        throw new Error("Please accept the terms and conditions");
+      }
+      return true;
+    }),
+
+  // Request handler
+  async (req, res) => {
+    const { firstName, lastName, email, password, terms } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).render("signup", {
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email },
+        user: {},
+      });
+    }
+
+    try {
+      const termsAccepted = terms === "on";
+
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+
+      // Save user
       const newUser = new UserModel({
-        name,
+        firstName,
+        lastName,
         email,
         password: hashPassword,
+        termsAccepted,
       });
+
       await newUser.save();
       res.redirect("/");
-    });
-  });
-});
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
+  }
+);
 app.post("/login", async (req, res) => {
   const { email } = req.body;
   let UserPresent = await UserModel.findOne({ email });
   if (!UserPresent) {
-    console.log("User Not Present");
-    res.redirect("/signup");
+    console.error("User Not Present");
+    res.render("login", {
+      errorMag: "User not found. Please check your email or sign up.",
+    });
   } else {
     bcrypt.compare(
       req.body.password,
@@ -82,15 +165,20 @@ app.post("/login", async (req, res) => {
           res.cookie("token", token);
           return res.redirect("/todos");
         } else {
-          console.log("PassWord Wrong!");
+          console.error("PassWord Wrong!");
+          // return res.redirect("/login");
+          res.render("login", {
+            errorMag: "Password is worng",
+            email,
+          });
         }
       }
     );
   }
 });
-app.get("/delete/:todoId",isLoggedIn, async (req, res) => {
+app.get("/delete/:todoId", isLoggedIn, async (req, res) => {
   const todoId = req.params.todoId;
-  const deletedTodo = await TodoModel.findOneAndDelete({ _id: todoId });
+  await TodoModel.findOneAndDelete({ _id: todoId });
   res.redirect("/todos");
 });
 app.get("/edit/:todoId", isLoggedIn, async (req, res) => {
@@ -101,7 +189,11 @@ app.get("/edit/:todoId", isLoggedIn, async (req, res) => {
   const todos = await TodoModel.find({ user: loggedInUser[0]._id }).populate(
     "user"
   );
-  res.render("todos", { editTodo, todos, userName: loggedInUser[0].name });
+  res.render("todos", {
+    editTodo,
+    todos,
+    userFristName: loggedInUser[0].firstName,
+  });
 });
 app.post("/edit-todo/:todoId", async (req, res) => {
   const { todo, date } = req.body;
--- models/user.js
+++ models/user.js
@@ -1,28 +1,37 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
   },
   email: {
     type: String,
+    required: true,
+    unique: true, // ensures no duplicate emails
+    lowercase: true,
+    trim: true
   },
   password: {
     type: String,
+    required: true
+  },
+  termsAccepted: {       // store if user accepted terms
+    type: Boolean,
+    required: true,
+    default: false
   },
   date: {
     type: Date,
-    default: new Date,
-  },
-  // todos:[
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "Todo"
-  //   }
-  // ],
+    default: new Date()
+  }
 });
-
 // Create model
 const User = mongoose.model("User", userSchema);
 
